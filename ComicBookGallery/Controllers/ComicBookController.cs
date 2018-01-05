1709c788eddb2d5ea7759b1f3eb3835e40385441
Iskander_Ismagilov <isma.translator@gmail.com>
2018-01-05 23:38:47 +0500

    Updated ComicBooks.Detail veiw. Data moved from cshtml to Detail action method.

--- ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -7,9 +7,18 @@ using System.Web.Mvc;
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
-    {
+    {        
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Mans.";
+            ViewBag.IssueNum = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new []{
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"};
             return View();
         }
     }
