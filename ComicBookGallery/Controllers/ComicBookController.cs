235581011b7a2ab21ccdbfbd4864ab792548bb7f
Iskander_Ismagilov <isma.translator@gmail.com>
2018-01-06 18:58:03 +0500

    Updated ComicBooks controller with passed pararmeter of comic id to get certain comic.

--- ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,24 +9,17 @@ using System.Web.Mvc;
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
-    {        
-        public ActionResult Detail()
+    {
+        private ComicBookRepository _comicBookRepo = new ComicBookRepository();        
+        
+        public ActionResult Detail(int? id)
         {
-            var comicBook = new ComicBook()
+            if (id != null)
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Role = "Script",Name = "Dan Slott"},
-                    new Artist() {Role = "Pencils",Name = "Humberto Ramos"},
-                    new Artist() {Role = "Inks",Name = "Victor Olazaba"},
-                    new Artist() {Role = "Colors",Name = "Edgar Delgado"},
-                    new Artist() {Role = "Letters",Name = "Chris Eliopoulos"}
-                }
-            };
-            return View(comicBook);
+                ComicBook comicBook = _comicBookRepo.GetComicBook((int)id);
+                return View(comicBook);
+            }
+            return HttpNotFound();
         }
     }
 }
